e6c8776e3bd27e8fbd80b994ea2f98698e47f168
Ivan Žalac <izalac@icloud.com>
2025-03-15 17:22:09 +0100

    Added auto ssh fingerprint acceptance and removed direct login on jumphost for jump users

--- create-inv-hosts.sh
+++ create-inv-hosts.sh
@@ -3,4 +3,8 @@ echo Creating Ansible inventory file from lxc list
 lxc list -f compact -c n4 | sed '1d' | awk '{print $1" ansible_host="$2}' | tee inventory
 echo && echo Creating hosts file from lxc list
 lxc list -f compact -c n4 | sed '1d' | awk '{print $2" "$1}' | tee hosts
-
+echo && echo Accepting SSH fingerprints and testing ansible ssh access
+lxc list -f compact -c n4 | sed '1d' | awk '{print $2}' > ipaddresses.tmp
+for i in $(cat ipaddresses.tmp); do ssh -oStrictHostKeyChecking=accept-new ansible@$i hostname; done
+rm ipaddresses.tmp
+echo && echo Done.
